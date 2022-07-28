435a8e9a8f8a059f527779e674a25d3e744b65e2
Victor <matheus0588@outlook.com>
2022-07-28 19:05:59 -0300

    Feito requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,95 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import saveEmailAction from '../redux/actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disableBtn: true,
+    };
+  }
+
+  checkpass = () => {
+    const { password } = this.state;
+    const magicNumber = 6;
+    if (password.length >= magicNumber) {
+      return true;
+    }
+  }
+
+  checkmail = () => {
+    const { email } = this.state;
+    if (email.includes('@') && email.includes('.com')) { return true; }
+  }
+
+  onChange = ({ target }) => {
+    const { value, type } = target;
+    this.setState({ [type]: value }, () => {
+      if (this.checkmail() === true && this.checkpass() === true) {
+        this.setState({
+          disableBtn: false,
+        });
+      } else {
+        this.setState({
+          disableBtn: true,
+        });
+      }
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { disableBtn, email, password } = this.state;
+    const { history, saveEmailState } = this.props;
+    return (
+      <div>
+        Login
+        <form>
+          <label htmlFor="email">
+            email:
+            <input
+              data-testid="email-input"
+              value={ email }
+              type="email"
+              onChange={ this.onChange }
+            />
+          </label>
+          <label htmlFor="password">
+            senha:
+            <input
+              data-testid="password-input"
+              value={ password }
+              type="password"
+              onChange={ this.onChange }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ disableBtn }
+            onClick={ () => {
+              saveEmailState(email);
+              history.push('/carteira');
+            } }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+  saveEmailState: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  saveEmailState: (email) => dispatch(saveEmailAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,2 +1,8 @@
-// Coloque aqui suas actions
-// initial commit
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+
+const saveEmailAction = (email) => ({
+  type: SAVE_EMAIL,
+  email,
+});
+
+export default saveEmailAction;
